c2e933798c1ca7e4a3837c0d463fd24e4d252e85
Simon Corless <sicouk@gmail.com>
2021-03-04 20:08:09 +0000

    Display the users copying layout from in this discussion module

--- views/modules/newmembers.php
+++ views/modules/newmembers.php
@@ -2,17 +2,20 @@
 $newMembers = $this->data('NewMembers');
 ?>
 
-<div id="NewMembers" class="Box">
-    <h4 aria-level="2"><?php echo t('New Members'); ?>
-        <span class="Count"><?php echo Gdn_Format::bigNumber(count($newMembers), 'html') ?></span>
-    </h4>
+<div class="Box BoxNewUsers">
+
+    <?php echo panelHeading(t('New Members')); ?>
     <?php
     if (count($newMembers) > 0) {
-        echo '<ul class="PanelInfo">';
+        echo '<ul class="PanelInfo PanelNewUsers">';
 
         foreach ($newMembers AS $user) {
-            //echo '<li>'.userAnchor($user, ($user['Invisible']) ? 'Invisible' : '').'</li>';
-            echo '<li>' . $user['id'] . '</li>';
+            echo '<li>' . anchor(
+                wrap(wrap(Gdn_Format::date($user->DateLastActive, 'html')), 'span', ['class' => 'Aside']).' '.
+                wrap(wrap(htmlspecialchars(val('Name', $user)), 'span', ['class' => 'Username']), 'span'),
+                userUrl($user)
+            ) . '</li>';
+            //echo '<li>' . userAnchor($user) . Gdn_Format::date($user->DateInserted, 'html') . '</li>';
         }
 
         echo '</ul>';
